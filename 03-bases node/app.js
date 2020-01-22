b0cbfeec8b60490c471b536fa713eadecbd4364b
Eliazar Alejandro <liazcortez@gmail.com>
2020-01-22 17:55:33 -0600

    update :v

--- 03-bases node/app.js
+++ 03-bases node/app.js
@@ -1,9 +1,16 @@
 //requireds
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de', {
+        base: { demand: true, alias: 'b' },
+        limite: { demand: true, alias: 'l', default: 10 }
+
+    }).help().argv;
 const { crearArchivo: multiplicar } = require('./multiplicar/multiplicar');
 // const fs = require('fs');
-let base = 'd';
 
 
+
+let base = argv.base;
 
 
 multiplicar(base).then(archivo => {
